f7685f946a30efa5580ab34df0549f301b0202cb
DebabrataBanik <debabratabanik02@gmail.com>
2024-12-19 23:31:37 +0530

    conditionally add and render ingredients

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,42 @@
+import { useState } from "react"
+
 function Main() {
+
+  const [ingredients, setIngredients] = useState([])
+
+  function addIngredient(form) {
+    const ingredient = form.get('ingredient')
+
+    if (ingredient.trim()) {
+      setIngredients(prev => [...prev, ingredient])
+    }
+
+  }
+
   return (
     <main className="container">
-      <form className="form__container">
+      <form action={addIngredient} className="form__container">
         <label hidden htmlFor="input"></label>
-        <input type="text" id="input" placeholder="e.g. oregano"
+        <input type="text" name="ingredient" id="input" placeholder="e.g. oregano"
           aria-label="add ingredient"
         />
         <button id="submit">+ Add ingredient</button>
       </form>
+
+      {
+        ingredients.length > 0 && (
+          <section className="ingredients__container">
+            <h2>Ingredients on hand:</h2>
+            <ul>
+              {
+                ingredients.map(each => (
+                  <li key={each}>{each}</li>
+                ))
+              }
+            </ul>
+          </section>
+        )
+      }
     </main>
   )
 }
