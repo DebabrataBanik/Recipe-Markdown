d09762dc12eac966f73bae1a0bbae214178ae49f
DebabrataBanik <debabratabanik02@gmail.com>
2024-12-20 23:46:01 +0530

    conditionally render ingredients

---
+++ src/components/Ingredients/IngredientsList.jsx
@@ -0,0 +1,30 @@
+function IngredientsList({
+  ingredients = [],
+  getRecipe
+}) {
+  return (
+    <section className="ingredients__container">
+      <h2>Ingredients on hand:</h2>
+      <ul>
+        {
+          ingredients.map(each => (
+            <li key={each}>{each}</li>
+          ))
+        }
+      </ul>
+      {
+        ingredients.length > 3 && (
+          <div className="get__recipe">
+            <aside className="ready">
+              <h3>Ready for a recipe?</h3>
+              <p>Generate a recipe from your list of ingredients.</p>
+            </aside>
+            <button className="btn" onClick={getRecipe}>Get a recipe</button>
+          </div>
+        )
+      }
+    </section>
+  )
+}
+
+export default IngredientsList
